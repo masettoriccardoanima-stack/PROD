436461c9b7c26785d8b94b1d900495bd94a9aea7
masettoriccardoanima-stack <masettoriccardoanima@gmail.com>
2025-12-02 19:14:39 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -11488,17 +11488,24 @@ const counters0 = lsGet('counters', {}) || {};
       ...appPrev,
       updatedAt: nowIso,
       // --- dati anagrafici base ---
-      ragioneSociale: form.ragioneSociale,
-      indirizzo     : form.indirizzo,
-      cap           : form.cap,
-      citta         : form.citta,
-      provincia     : form.provincia,
-      nazione       : form.nazione,
-      piva          : form.piva,
-      codiceFiscale : form.codiceFiscale,
-      emailFatture  : form.emailFatture,
-      telefono      : form.telefono,
-      sitoWeb       : form.sitoWeb,
+      ragioneSociale   : form.ragioneSociale,
+      indirizzo        : form.indirizzo,
+      cap              : form.cap,
+      citta            : form.citta,
+      provincia        : form.provincia,
+      nazione          : form.nazione,
+      piva             : form.piva,
+      codiceFiscale    : form.codiceFiscale,
+
+      // Dati azienda aggiuntivi (usati nelle intestazioni stampe)
+      sedeLegale       : form.sedeLegale,
+      sedeOperativa    : form.sedeOperativa,
+      email            : form.email,
+
+      // Contatti fatturazione / sito
+      emailFatture     : form.emailFatture,
+      telefono         : form.telefono,
+      sitoWeb          : form.sitoWeb,
 
       // Dati fiscali aggiuntivi
       rea            : form.rea,
@@ -11561,7 +11568,9 @@ const counters0 = lsGet('counters', {}) || {};
       publicBaseUrl,
 
       // Altre opzioni
-      magUpdateCMP   : magUpdateCMP,
+      magUpdateCMP        : magUpdateCMP,
+      publicBaseUrl       : publicBaseUrl,
+      costoOrarioAzienda  : Number(form.costoOrarioAzienda) || 0,
 
       // Logo
       logoDataUrl     : form.logoDataUrl || app0.logoDataUrl || '',
